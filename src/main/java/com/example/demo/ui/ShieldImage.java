21571b8a368d4a9e39d0a65178067615f9197f4a
pegasus9898 <163820538+pegasus9898@users.noreply.github.com>
2024-11-22 19:13:40 +0800

    Fixed Shield Image issues by adding a flag to track if shield was on or not and implemented a updatePosition function to help track the shield better

---
+++ src/main/java/com/example/demo/ui/ShieldImage.java
@@ -0,0 +1,49 @@
+package com.example.demo.ui;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class ShieldImage extends ImageView {
+	
+	//private static final String IMAGE_NAME = "/images/shield.png";
+	private static final int SHIELD_SIZE = 200;
+	private boolean isShieldOn = false; // Track the shield state (on/off)
+	
+	public ShieldImage(double xPosition, double yPosition) {
+		this.setLayoutX(xPosition);
+		this.setLayoutY(yPosition);
+		//this.setImage(new Image(IMAGE_NAME));
+		this.setImage(new Image(getClass().getResource("/com/example/demo/images/shield.png").toExternalForm()));
+		//this.setImage(new Image(getClass().getResource("/com/example/demo/images/*" + IMAGE_NAME).toExternalForm()));
+		this.setVisible(true);
+		this.setFitHeight(SHIELD_SIZE);
+		this.setFitWidth(SHIELD_SIZE);
+	}
+
+	public void showShield() {
+		if (!isShieldOn) {
+			isShieldOn = true;
+			this.setVisible(true); // Show shield
+			System.out.println("Shield Position(in shield image class): " + getLayoutX() + ", " + getLayoutY());
+		}
+
+
+
+	}
+	
+	public void hideShield() {
+
+			this.setVisible(false); // Hide shield
+			System.out.println("Shield Off shield image class");
+
+	}
+
+	// Method to update the shield position when moving the plane
+	public void updatePosition(double xPosition, double yPosition) {
+		setLayoutX(xPosition);
+		setLayoutY(yPosition);
+		System.out.println("Shield Position Updated(Shield Image class): " + getLayoutX() + ", " + getLayoutY());
+
+	}
+
+}
