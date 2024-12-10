23b154c0abd70b2162e3ddbf890a4d9c5f24046b
pegasus9898 <163820538+pegasus9898@users.noreply.github.com>
2024-12-11 01:13:15 +0800

    -added two audio wav files for user projectile sound and background music -changed dependencies for javafx-media to work in pom.xml and module-info.java (gave me a hard time but worked well in the end) -added banners' classes into a package that is located in the "levels" package itself -moved all level View classes into a 'views' package which is located in the "levels" package itself -moved all level 4 enemies in a 'l4enemies' located in the "actors" package itself -moved all level 3 enemies in a 'l3enemies'  located in the "actors" package itself -moved Collision class into a 'physics' package -i am only commiting the module-info.java, not the classes in which they have changed the packages -i will commit the actual change in packages in the next commit -added pause menu and restart window classes into a 'menus' package -deleted Destructible.java interface as it was not serving any purpose

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,18 @@ module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires javafx.media;
 
-    opens com.example.demo to javafx.fxml;
+    opens com.example.demo.controller to javafx.fxml;
     exports com.example.demo.controller;
     opens com.example.demo.actors to javafx.fxml;
     opens com.example.demo.projectiles to javafx.fxml;
     opens com.example.demo.ui to javafx.fxml;
     opens com.example.demo.levels to javafx.fxml;
+    opens com.example.demo.levels.banners to javafx.fxml;
+    opens com.example.demo.levels.views to javafx.fxml;
+    opens com.example.demo.actors.l4enemies to javafx.fxml;
+    opens com.example.demo.actors.l3enemies to javafx.fxml;
+    opens com.example.demo.physics to javafx.fxml;
+    opens com.example.demo.menus to javafx.fxml;
 }
